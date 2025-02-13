aef541419a2b309766546fcc0878d7bca409893f
Adriano Harm <adhkr81@gmail.com>
2025-02-13 09:43:09 -0600

    added video urls to dictionary

--- dictionary.js
+++ dictionary.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 function createDictionary() {
   const tempFilePath = path.join(__dirname, "_temp", "new_names.json");
+  const imageExtensions = ["jpg", "jpeg", "png", "gif", "bmp", "webp", "svg",];
+  const videoExtensions = ["mp4", "mov", "avi", "mkv", "webm"];
 
   try {
     const newNamesData = JSON.parse(fs.readFileSync(tempFilePath, "utf-8"));
@@ -12,7 +14,8 @@ function createDictionary() {
 
     const dictionary = assetsFromCollection.reduce((acc, item) => {
 
-    const key = `/${newNamesData[item.name]["path"]}/${newNamesData[item.name]["originalName"]}`
+    const key = `/${newNamesData[item.name]["path"]}`
+    const extension = item.extension[0]
 
       if (newNamesData[item.name]) {
         let lowerCaseId = item.id.toLowerCase(); // Convert hash to lowercase
@@ -24,7 +27,15 @@ function createDictionary() {
         }
         const formattedId = parts.join("-");
 
-        acc[key] = `https://media.us.lg.com/transform/${formattedId}/${item.name}`;
+        if (imageExtensions.includes(extension)) {
+          acc[key] = `https://media.us.lg.com/transform/${formattedId}/${item.name}`;
+        } else if (videoExtensions.includes(extension)) {
+          acc[key] = `https://media.us.lg.com/asset/${formattedId}/${extension}/${item.name}`
+        } else {
+          console.log("File extension not recognized; please add logic to handle this case. :" , item.name, extension)
+          
+        }
+
       } else {
         console.warn(`Warning: No mapping found for ${item.name}`);
       }
